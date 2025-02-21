eff0695a80f9552dd5bfe6a62aee3dca69105cbe
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2025-02-21 14:50:01 +0300

    💎 KANAMBO TECH 💎

--- pair.js
+++ pair.js
@@ -1,107 +1,90 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
-const { makeid } = require('./id');
-const express = require('express');
-const fs = require('fs');
-let router = express.Router();
-const pino = require("pino");
-const {
-    default: Wasi_Tech,
-    useMultiFileAuthState,
-    delay,
-    makeCacheableSignalKeyStore,
-    Browsers
-} = require("maher-zubair-baileys");
+const PastebinAPI = require('pastebin-js'), pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL'); const { makeid } = require('./id'); const express = require('express'); const fs = require('fs'); let router = express.Router(); const pino = require("pino"); const { default: Wasi_Tech, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, Browsers } = require("maher-zubair-baileys");
 
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
-}
+function removeFile(FilePath) { if (!fs.existsSync(FilePath)) return false; fs.rmSync(FilePath, { recursive: true, force: true }); }
 
-router.get('/', async (req, res) => {
-    const id = makeid();
-    let num = req.query.number;
+router.get('/', async (req, res) => { const id = makeid(); let num = req.query.number;
 
-    async function WASI_MD_PAIR_CODE() {
-        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
-        
-        try {
-            let Pair_Code_By_Wasi_Tech = Wasi_Tech({
-                auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
-                },
-                printQRInTerminal: false,
-                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: ["Chrome (Linux)", "", ""]
-            });
+async function WASI_MD_PAIR_CODE() {
+    const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+    
+    try {
+        let Pair_Code_By_Wasi_Tech = Wasi_Tech({
+            auth: {
+                creds: state.creds,
+                keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+            },
+            printQRInTerminal: false,
+            logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+            browser: ["Chrome (Linux)", "", ""]
+        });
 
-            if (!Pair_Code_By_Wasi_Tech.authState.creds.registered) {
-                await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await Pair_Code_By_Wasi_Tech.requestPairingCode(num);
-                
-                if (!res.headersSent) {
-                    await res.send({ code });
-                }
-            }
-
-            Pair_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds);
-            Pair_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
-
-                if (connection == "open") {
-                    await delay(5000);
-                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                    await delay(800);
-                    let b64data = Buffer.from(data).toString('base64');
-                    let session = await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
-
-                    // Image URL
-                    const imageUrl = "https://files.catbox.moe/dcoxvf.jpg";
-
-                    // WhatsApp Message Text
-                    let WASI_MD_TEXT = `
-*Session Connected*  
----------------------------  
-
-📱 *Follow the channel for bot updates:*  
-https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y  
-
-🕹 *Follow GitHub:*  
-https://github.com/Kanambp/dreaded-v2  
-
-🌐 *For more info, visit:*  
-https://kanambotech.com  
-
-😎 *Made by Kanambo Tech*
-`;
-
-                    const messageOptions = {
-                        image: { url: imageUrl },
-                        caption: WASI_MD_TEXT
-                    };
-
-                    await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, messageOptions, { quoted: session });
-
-                    await delay(100);
-                    await Pair_Code_By_Wasi_Tech.ws.close();
-                    return await removeFile('./temp/' + id);
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(10000);
-                    WASI_MD_PAIR_CODE();
-                }
-            });
-        } catch (err) {
-            console.log("Service restarted");
-            await removeFile('./temp/' + id);
+        if (!Pair_Code_By_Wasi_Tech.authState.creds.registered) {
+            await delay(1500);
+            num = num.replace(/[^0-9]/g, '');
+            const code = await Pair_Code_By_Wasi_Tech.requestPairingCode(num);
+            
             if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
+                await res.send({ code });
             }
         }
+
+        Pair_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds);
+        Pair_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+            const { connection, lastDisconnect } = s;
+
+            if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+                let b64data = Buffer.from(data).toString('base64');
+                let session = await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+
+                // Image URL
+                const imageUrl = "https://files.catbox.moe/dcoxvf.jpg";
+
+                // WhatsApp Message Text
+                let WASI_MD_TEXT = `
+
+Session Connected
+
+📱 Follow the channel for bot updates:
+https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y
+
+🕹 Follow GitHub:
+https://github.com/Kanambp/dreaded-v2
+
+🌐 For more info, visit:
+https://kanambotech.com
+
+😎 Made by Kanambo Tech `;
+
+const messageOptions = {
+                    image: { url: imageUrl },
+                    caption: WASI_MD_TEXT
+                };
+
+                await Pair_Code_By_Wasi_Tech.sendMessage(Pair_Code_By_Wasi_Tech.user.id, messageOptions, { quoted: session });
+
+                await delay(100);
+                await Pair_Code_By_Wasi_Tech.ws.close();
+                return await removeFile('./temp/' + id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                await delay(10000);
+                WASI_MD_PAIR_CODE();
+            }
+        });
+    } catch (err) {
+        console.log("Service restarted");
+        await removeFile('./temp/' + id);
+        if (!res.headersSent) {
+            await res.send({ code: "Service Unavailable" });
+        }
     }
-    
-    return await WASI_MD_PAIR_CODE();
+}
+
+return await WASI_MD_PAIR_CODE();
+
 });
 
 module.exports = router;
+
