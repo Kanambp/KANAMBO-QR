6ae53d12521eb2e1a4b31fac3637610b8c13e7d4
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2025-02-22 06:36:31 +0300

    Update kanamboqr.js

--- kanamboqr.js
+++ kanamboqr.js
@@ -1,108 +1,68 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
-const QRCode = require('qrcode');
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-let router = express.Router()
-const pino = require("pino");
-const {
-	default: Wasi_Tech,
-	useMultiFileAuthState,
-	jidNormalizedUser,
-	Browsers,
-	delay,
-	makeInMemoryStore,
-} = require("@whiskeysockets/baileys");
+const PastebinAPI = require('pastebin-js'), pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL') const {makeid} = require('./id'); const QRCode = require('qrcode'); const express = require('express'); const path = require('path'); const fs = require('fs'); let router = express.Router() const pino = require("pino"); const { default: Wasi_Tech, useMultiFileAuthState, jidNormalizedUser, Browsers, delay, makeInMemoryStore, } = require("@whiskeysockets/baileys");
 
-function removeFile(FilePath) {
-	if (!fs.existsSync(FilePath)) return false;
-	fs.rmSync(FilePath, {
-		recursive: true,
-		force: true
-	})
-};
-const {
-	readFile
-} = require("node:fs/promises")
-router.get('/', async (req, res) => {
-	const id = makeid();
-	async function WASI_MD_QR_CODE() {
-		const {
-			state,
-			saveCreds
-		} = await useMultiFileAuthState('./temp/' + id)
-		try {
-			let Qr_Code_By_Wasi_Tech = Wasi_Tech({
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
+function removeFile(FilePath) { if (!fs.existsSync(FilePath)) return false; fs.rmSync(FilePath, { recursive: true, force: true }) }; const { readFile } = require("node:fs/promises") router.get('/', async (req, res) => { const id = makeid(); async function WASI_MD_QR_CODE() { const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id) try { let Qr_Code_By_Wasi_Tech = Wasi_Tech({ auth: state, printQRInTerminal: false, logger: pino({ level: "silent" }), browser: Browsers.macOS("Desktop"), });
 
-			Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
-				const {
-					connection,
-					lastDisconnect,
-					qr
-				} = s;
-				if (qr) await res.end(await QRCode.toBuffer(qr));
-				if (connection == "open") {
-					await delay(5000);
-					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-					await delay(800);
-				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
-                                   let WASI_MD_TEXT = `
-🌟 *Session Successfully Connected via KANAMBO!* 👌  
-❤️ *Built with Passion*  
+Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
+		Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+			const {
+				connection,
+				lastDisconnect,
+				qr
+			} = s;
+			if (qr) await res.end(await QRCode.toBuffer(qr));
+			if (connection == "open") {
+				await delay(5000);
+				let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+				await delay(800);
+			   let b64data = Buffer.from(data).toString('base64');
+			   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+			   let WASI_MD_TEXT = `
 
-━━━━━━━━━━━━━━━━━━━  
+🌟 Session Successfully Connected via KANAMBO! 👌
+❤️ Built with Passion
 
-🔹 *『 GREAT CHOICE – WELCOME TO KANAMBO MD! 』*  
-_You’ve successfully completed the first step to deploying your WhatsApp bot._ 🚀  
+━━━━━━━━━━━━━━━━━━━
 
-━━━━━━━━━━━━━━━━━━━  
+🔹 『 GREAT CHOICE – WELCOME TO KANAMBO MD! 』
+You’ve successfully completed the first step to deploying your WhatsApp bot. 🚀
 
-📌 *Need Help? Find Us Here:*  
-➤ 👤 *Owner:* [Chat Now](https://wa.me/+254114148625)  
-➤ 🛠 *GitHub Repo:* [View Project](https://github.com/Kanambp/dreaded-v2)  
-➤ 🤝 *WhatsApp Group:* [Join Us](https://chat.whatsapp.com/Byx7wdqizJXB79RKFKsefb)  
+━━━━━━━━━━━━━━━━━━━
 
-━━━━━━━━━━━━━━━━━━━  
+📌 Need Help? Find Us Here:
+➤ 👤 Owner: Chat Now
+➤ 🛠 GitHub Repo: View Project
+➤ 🤝 WhatsApp Group: Join Us
 
-💡 *Thank you for choosing KANAMBO TECH!*  
-🙏 *Please keep your session private.*  
+━━━━━━━━━━━━━━━━━━━
 
-✨ _Don't forget to star my repo on GitHub!_ ⭐  
+💡 Thank you for choosing KANAMBO TECH!
+🙏 Please keep your session private.
+
+✨ Don't forget to star my repo on GitHub! ⭐
 `;
 
 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: WASI_MD_TEXT }, { quoted: session });
 
-
-					await delay(100);
-					await Qr_Code_By_Wasi_Tech.ws.close();
-					return await removeFile("temp/" + id);
-				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-					await delay(10000);
-					WASI_MD_QR_CODE();
-				}
-			});
-		} catch (err) {
-			if (!res.headersSent) {
-				await res.json({
-					code: "Service is Currently Unavailable"
-				});
+await delay(100);
+				await Qr_Code_By_Wasi_Tech.ws.close();
+				return await removeFile("temp/" + id);
+			} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+				await delay(10000);
+				WASI_MD_QR_CODE();
 			}
-			console.log(err);
-			await removeFile("temp/" + id);
+		});
+	} catch (err) {
+		if (!res.headersSent) {
+			await res.json({
+				code: "Service is Currently Unavailable"
+			});
 		}
+		console.log(err);
+		await removeFile("temp/" + id);
 	}
-	return await WASI_MD_QR_CODE()
-});
-module.exports = router
-		
+}
+return await WASI_MD_QR_CODE()
+
+}); module.exports = router;
+
+										      
